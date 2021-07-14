94d9b3e38376ddbc5fba871180caeaee6f745528
Zijian Yue <zijian.yue@student.unsw.edu.au>
2021-07-15 06:38:32 +1000

    Upload New File

---
+++ src/unsw/loopmania/Items/Staff.java
@@ -0,0 +1,28 @@
+package unsw.loopmania.items;
+
+public class Staff extends BasicItem {
+    //Constructor
+    /*
+      @param dropRate ----> the rate of dropping of an item when an enemy is defeated 
+      @param type ----> the type of the item
+      @param purchasePrice -----> the price for purhcase this item
+      note: the sell price for this item will be 1/2 of the pruchase price
+    */
+    public Staff(int dropRate, String type, int purchasePrice)
+    {
+        super(dropRate,type,purchasePrice);
+    }
+    /*
+        equip and unequip
+    */
+    @Override
+    public void display() 
+    {
+        return;
+    }
+    @Override
+    public Boolean isApplicable()
+    {
+        return false;
+    }
+}
