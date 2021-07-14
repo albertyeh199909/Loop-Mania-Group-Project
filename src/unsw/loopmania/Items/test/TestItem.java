95df919f47c2e4513ca402f7a27a9dd32bb99ab8
Zijian Yue <zijian.yue@student.unsw.edu.au>
2021-07-15 00:16:06 +1000

    half way through the basic item testings !!!!!!!

--- src/unsw/loopmania/Items/test/TestItem.java
+++ src/unsw/loopmania/Items/test/TestItem.java
@@ -30,4 +30,45 @@ public class TestItem {
         assertEquals(test_gold.getGold(), 100);
 
     }
+     @Test
+    public void testCreate()
+    {
+        ArrayList<BasicItem> basicItem = new ArrayList<BasicItem>();
+
+        // create instance for Armour, Shield, Helmet, Staff ,Sword, Stake, Potion
+        // test wheather these basic items can be added to one BasicItem(abstarct class) array
+        Armour A = new Armour(20, "Armour", 100);
+        Shield B = new Shield(21, "Shield", 200);
+        Helmet C = new Helmet(22, "Helmet", 300);
+        Staff D = new Staff(23,"Staff", 400);
+        Sword E = new Sword(24,"Sword", 500);
+        Stake F = new Stake(25,"Stake",600);
+        Potion G = new Potion(26,"Potion", 700);
+
+        //put everything into an array
+        basicItem.add(A);
+        basicItem.add(B);
+        basicItem.add(C);
+        basicItem.add(D);
+        basicItem.add(E);
+        basicItem.add(F);
+        basicItem.add(G);
+
+        //check up on everthing !!!!
+        
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+    }
 }
