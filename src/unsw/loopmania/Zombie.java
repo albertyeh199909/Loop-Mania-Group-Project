4313e33e33fdd6d434f1c691c212561c48078feb
s2424361895 <michael.j.shi@outlook.com>
2021-07-14 22:58:08 +1000

    implemented zombie inflict damage method

--- src/unsw/loopmania/Zombie.java
+++ src/unsw/loopmania/Zombie.java
@@ -1,13 +1,25 @@
 package unsw.loopmania;
 
+import java.util.Random;
+
 public class Zombie extends BasicEnemy {
     public Zombie(PathPosition pathPosition) {
         super(pathPosition, 10, 4, 10, 2);
     }
 
     @Override
-    public void inflictDamage() {}
+    public void inflictDamage() {
+        if (movingEntity instanceof AlliedSoldier) {
+            int randomInt = new Random().nextInt(100);
+            if (randomInt <= 10) {
+                // Turn alliedSoldier to zombie
+            } else {
+                super.inflictDamage();
+            }
 
-    @Override
-    public void takeDamage() {}
+        } else {
+            super.inflictDamage();
+        }
+    }
+
 }
