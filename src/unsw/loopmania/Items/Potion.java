cd0f8f8332c7e1791a75d81b04d7c6b3e6b1eb26
Zijian Yue <zijian.yue@student.unsw.edu.au>
2021-07-15 06:38:02 +1000

    Upload Baisc Items!!!

---
+++ src/unsw/loopmania/Items/Potion.java
@@ -0,0 +1,34 @@
+package unsw.loopmania.items;
+
+public class Potion extends BasicItem {
+    //Constructor
+    /*
+      @param dropRate ----> the rate of dropping of an item when an enemy is defeated 
+      @param type ----> the type of the item
+      @param purchasePrice -----> the price for purhcase this item
+      note: the sell price for this item will be 1/2 of the pruchase price
+    */
+    public Potion(int dropRate, String type, int purchasePrice)
+    {
+        super(dropRate,type,purchasePrice);
+    }
+    /*
+        equip and unequip
+    */
+    @Override
+    public void display() 
+    {
+        return;
+    }
+    @Override
+    public Boolean isApplicable()
+    {
+        return true;
+    }
+
+    @Override
+    public void useItem(Character c)
+    {
+        //c.setHealth(20);
+    }
+}
