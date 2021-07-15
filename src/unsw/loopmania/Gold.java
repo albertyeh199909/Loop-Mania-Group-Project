f871e66ae29e11eb8fcc0d4386ae12752659d457
Zijian Yue <zijian.yue@student.unsw.edu.au>
2021-07-16 07:41:20 +1000

    Update Gold to make sure gold is an entity as well

--- src/unsw/loopmania/Gold.java
+++ src/unsw/loopmania/Gold.java
@@ -1,10 +1,19 @@
 package unsw.loopmania;
 
-public class Gold
+import javafx.beans.property.SimpleIntegerProperty;
+
+public class Gold extends Entity
 {
     int number;
+    PathPosition position;
 
-    public Gold(int initNum)
+    //Constructor
+    /*
+        @initNum is the initial gold that the main character should have
+        @param postion is the postion of the current item
+        note: the sell price for this item will be 1/2 of the pruchase price
+    */
+    public Gold(int initNum, PathPosition position)
     {
         this.number = initNum;
     }
@@ -25,5 +34,22 @@ public class Gold
     public void display()
     {
 
+    }
+
+     // implement the abstract functions in entites class
+    public SimpleIntegerProperty x() {
+        return position.getX();
+    }
+
+    public SimpleIntegerProperty y() {
+        return position.getY();
+    }
+
+    public int getX() {
+        return x().get();
+    }
+
+    public int getY() {
+        return y().get();
     }
 }
