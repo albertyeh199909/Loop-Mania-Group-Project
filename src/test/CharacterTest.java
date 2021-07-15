10c5f7adb88cd23c92781d2fa298a8d500f8d48e
Zijian Yue <zijian.yue@student.unsw.edu.au>
2021-07-16 07:07:17 +1000

    Update CharacterTest class to adopt the new item class !

--- src/test/CharacterTest.java
+++ src/test/CharacterTest.java
@@ -27,16 +27,16 @@ public class CharacterTest {
     @Test
     public void testMove() {
         List<Pair<Integer, Integer>> path = new ArrayList<Pair<Integer, Integer>>();
-        path.add(new Pair(4,3));
-        path.add(new Pair(4,2));
-        path.add(new Pair(3,2));
-        path.add(new Pair(3,1));
-        path.add(new Pair(2,1));
-        path.add(new Pair(1,1));
-        path.add(new Pair(1,2));
-        path.add(new Pair(1,3));
-        path.add(new Pair(2,3));
-        path.add(new Pair(3,3));
+        path.add(new Pair<Integer, Integer>(4,3));
+        path.add(new Pair<Integer, Integer>(4,2));
+        path.add(new Pair<Integer, Integer>(3,2));
+        path.add(new Pair<Integer, Integer>(3,1));
+        path.add(new Pair<Integer, Integer>(2,1));
+        path.add(new Pair<Integer, Integer>(1,1));
+        path.add(new Pair<Integer, Integer>(1,2));
+        path.add(new Pair<Integer, Integer>(1,3));
+        path.add(new Pair<Integer, Integer>(2,3));
+        path.add(new Pair<Integer, Integer>(3,3));
 
         //LoopManiaWorld world = new LoopManiaWorld(5,5, path);
         PathPosition start = new PathPosition(2, path);
@@ -55,20 +55,20 @@ public class CharacterTest {
     @Test
     public void testInventory() {
         List<Pair<Integer, Integer>> path = new ArrayList<Pair<Integer, Integer>>();
-        path.add(new Pair(4,3));
+        path.add(new Pair<Integer, Integer>(4,3));
         
 
         //LoopManiaWorld world = new LoopManiaWorld(5,5, path);
         PathPosition start = new PathPosition(0, path);
         Character player = new Character(start);
-        Sword sword = new Sword(20, "sword", 300);
+        Sword sword = new Sword(20, "sword", 300,null);
         player.store(sword);
         assertTrue(player.getInventory().contains(sword));
         player.setWeapon(sword);
         assertFalse(player.getInventory().contains(sword));
         assertEquals(player.getWeapon(), sword);
         assertTrue(player.getDamage() instanceof SwordStrategy);
-        Potion potion = new Potion(10, "potion", 10);
+        Potion potion = new Potion(10, "potion", 10,null);
         player.store(potion);
         //sword is equipped so it's not in inventory
         assertFalse(player.getInventory().contains(sword));
@@ -83,16 +83,16 @@ public class CharacterTest {
     public void testDamage() {
         
         List<Pair<Integer, Integer>> path = new ArrayList<Pair<Integer, Integer>>();
-        path.add(new Pair(4,3));
-        path.add(new Pair(4,2));
-        path.add(new Pair(3,2));
-        path.add(new Pair(3,1));
-        path.add(new Pair(2,1));
-        path.add(new Pair(1,1));
-        path.add(new Pair(1,2));
-        path.add(new Pair(1,3));
-        path.add(new Pair(2,3));
-        path.add(new Pair(3,3));
+        path.add(new Pair<Integer, Integer>(4,3));
+        path.add(new Pair<Integer, Integer>(4,2));
+        path.add(new Pair<Integer, Integer>(3,2));
+        path.add(new Pair<Integer, Integer>(3,1));
+        path.add(new Pair<Integer, Integer>(2,1));
+        path.add(new Pair<Integer, Integer>(1,1));
+        path.add(new Pair<Integer, Integer>(1,2));
+        path.add(new Pair<Integer, Integer>(1,3));
+        path.add(new Pair<Integer, Integer>(2,3));
+        path.add(new Pair<Integer, Integer>(3,3));
 
         //LoopManiaWorld world = new LoopManiaWorld(5,5, path);
         PathPosition start = new PathPosition(2, path);
@@ -106,7 +106,7 @@ public class CharacterTest {
         player.dealDamage(slug,array);
         assertEquals(2, slug.getHealth());
         //test stake damage against basic enemy and against vampire
-        BasicItem stake = new Stake(10,"stake",100);
+        BasicItem stake = new Stake(10,"stake",100,null);
         player.store(stake);
         player.setWeapon(stake);
         player.dealDamage(slug,array);
@@ -115,7 +115,7 @@ public class CharacterTest {
         assertEquals(4, vampire.getHealth());
         //test sword damage
         vampire.setHealth(10);
-        BasicItem sword = new Sword(10, "sword", 200);
+        BasicItem sword = new Sword(10, "sword", 200,null);
         player.store(sword);
         player.setWeapon(sword);
         player.dealDamage(vampire,array);
@@ -132,16 +132,16 @@ public class CharacterTest {
     @Test
     public void testDefence() {
         List<Pair<Integer, Integer>> path = new ArrayList<Pair<Integer, Integer>>();
-        path.add(new Pair(4,3));
-        path.add(new Pair(4,2));
-        path.add(new Pair(3,2));
-        path.add(new Pair(3,1));
-        path.add(new Pair(2,1));
-        path.add(new Pair(1,1));
-        path.add(new Pair(1,2));
-        path.add(new Pair(1,3));
-        path.add(new Pair(2,3));
-        path.add(new Pair(3,3));
+        path.add(new Pair<Integer, Integer>(4,3));
+        path.add(new Pair<Integer, Integer>(4,2));
+        path.add(new Pair<Integer, Integer>(3,2));
+        path.add(new Pair<Integer, Integer>(3,1));
+        path.add(new Pair<Integer, Integer>(2,1));
+        path.add(new Pair<Integer, Integer>(1,1));
+        path.add(new Pair<Integer, Integer>(1,2));
+        path.add(new Pair<Integer, Integer>(1,3));
+        path.add(new Pair<Integer, Integer>(2,3));
+        path.add(new Pair<Integer, Integer>(3,3));
 
         //LoopManiaWorld world = new LoopManiaWorld(5,5, path);
         PathPosition start = new PathPosition(2, path);
@@ -152,7 +152,7 @@ public class CharacterTest {
         Vampire vampire = new Vampire(below);
         
         //test helmet defence and damage reduction
-        Helmet helmet = new Helmet(10, "helmet", 100);
+        Helmet helmet = new Helmet(10, "helmet", 100,null);
         player.store(helmet);
         player.setHelmet(helmet);
         ArrayList<AlliedSoldier> array = new ArrayList<AlliedSoldier>();
@@ -164,7 +164,7 @@ public class CharacterTest {
         assertEquals(8,damage.getDamage());
 
         //test armor and armor damage reduction, armor should take priority over helmet for damage calculations
-        Armour armor = new Armour(10, "Armour", 100);
+        Armour armor = new Armour(10, "Armour", 100,null);
         player.store(armor);
         player.setArmor(armor);
 
@@ -173,7 +173,7 @@ public class CharacterTest {
         assertEquals(3,damage.getDamage());
 
         //test if shield reduces vampire crit chance by 60%
-        Shield shield = new Shield(10, "Shield", 100);
+        Shield shield = new Shield(10, "Shield", 100,null);
         player.store(shield);
         player.setShield(shield);
 
@@ -189,7 +189,7 @@ public class CharacterTest {
     @Test
     public void testInventoryLimit() {
         List<Pair<Integer, Integer>> path = new ArrayList<Pair<Integer, Integer>>();
-        path.add(new Pair(4,3));
+        path.add(new Pair<Integer, Integer>(4,3));
         
 
         //LoopManiaWorld world = new LoopManiaWorld(5,5, path);
@@ -197,9 +197,9 @@ public class CharacterTest {
         Character player = new Character(start);
         
         for(int i = 1; i < 17; i++) {
-            player.store(new Sword(20, "sword", i*100));
+            player.store(new Sword(20, "sword", i*100,null));
         }
-        player.store(new Sword(20, "sword", 1700));
+        player.store(new Sword(20, "sword", 1700,null));
         //assert player gets experience and gold, and oldest item disappears
         assertEquals(50, player.getExperience());
         assertEquals(25, player.getGold());
