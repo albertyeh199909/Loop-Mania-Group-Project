bfcae6e1f3383c3b0470c6797530be8da3c847b9
z5317144 <email>
2021-07-16 07:55:38 +1000

    Updated some buildings

--- src/unsw/loopmania/buildings/Tower.java
+++ src/unsw/loopmania/buildings/Tower.java
@@ -1,7 +1,20 @@
 package unsw.loopmania.buildings;
 
 public class Tower extends Building {
+    private int attack;
+    private int attackRadius;
+
     public Tower() {
         super("Tower", 3);
+        attack = 1;
+        attackRadius = 1;
+    }
+
+    public int getAttack() {
+        return attack;
+    }
+
+    public int getAttackRadius() {
+        return attackRadius;
     }
 }
