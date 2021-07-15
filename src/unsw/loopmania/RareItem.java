823e1a4236739173981d6bc89aa984b81ba00535
Zijian Yue <zijian.yue@student.unsw.edu.au>
2021-07-16 07:51:32 +1000

    Rare item should be an entity as well

--- src/unsw/loopmania/RareItem.java
+++ src/unsw/loopmania/RareItem.java
@@ -8,10 +8,12 @@ public abstract class RareItem extends Item {
     /*
       @param dropRate ----> the rate of dropping of an item when an enemy is defeated 
       @param type ----> the type of the item
+      @param postion is the postion of the current item
     */
-    public RareItem(int dropRate, String type)
+    public RareItem(int dropRate, String type, PathPosition position)
     {
          setDropRate(dropRate);
          setType(type);
+         setPosition(position);
     }
  }
