6e6e229e8dc4af3b6cc38bd2e7a80b97bde4d0fc
z5317144 <email>
2021-07-16 06:00:31 +1000

    Forgot to add

--- src/unsw/loopmania/buildings/Building.java
+++ src/unsw/loopmania/buildings/Building.java
@@ -4,6 +4,15 @@ public abstract class Building {
     private String type;
     private int placement;
     
+    /**
+     * 
+     * @param type      name of the building
+     * @param placement where it can be placed
+     *                  1 - Only on path tiles
+     *                  2 - Any non-path tile
+     *                  3 - Only on non-path tiles adjacent to the path
+     *                  4 - Exists at the starting position of the Character 
+     */
     public Building(String type, int placement) {
         this.type = type;
         this.placement = placement;
