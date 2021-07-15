64c448a32e84803a4c8598ad1bf96874fbe0ae97
Zijian Yue <zijian.yue@student.unsw.edu.au>
2021-07-16 07:49:05 +1000

    new basic item design which converts an item to an entity

--- src/unsw/loopmania/BasicItem.java
+++ src/unsw/loopmania/BasicItem.java
@@ -9,12 +9,14 @@ public abstract class BasicItem extends Item {
       @param dropRate ----> the rate of dropping of an item when an enemy is defeated 
       @param type ----> the type of the item
       @param purchasePrice -----> the price for purhcase this item
+      @param postion is the postion of the current item
       note: the sell price for this item will be 1/2 of the pruchase price
    */
-   public BasicItem(int dropRate, String type, int purchasePrice)
+   public BasicItem(int dropRate, String type, int purchasePrice,PathPosition position)
    {
         setDropRate(dropRate);
         setType(type);
         setPurchasePrice(purchasePrice);
+        setPosition(position);
    }
 }
