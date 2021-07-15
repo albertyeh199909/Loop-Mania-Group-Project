2dba34d26fe4942aa6e003faf261c1944305bd57
Tai-Jung Yeh <tai-jung.yeh@student.unsw.edu.au>
2021-07-15 18:58:39 +1000

    Update Zombie.java inflictDamage() to work with DamageClass

--- src/unsw/loopmania/Zombie.java
+++ src/unsw/loopmania/Zombie.java
@@ -3,22 +3,17 @@ package unsw.loopmania;
 import java.util.Random;
 
 public class Zombie extends BasicEnemy {
+    int criticalChance = 10;
     public Zombie(PathPosition pathPosition) {
         super(pathPosition, 10, 4, 10, 2);
     }
 
     @Override
     public void inflictDamage() {
-        if (movingEntity instanceof AlliedSoldier) {
-            int randomInt = new Random().nextInt(100);
-            if (randomInt <= 10) {
+        
+        DamageClass damage = entity.takeDamage(new DamageClass(this, damageValue, this.criticalChance));
+        if (movingEntity instanceof AlliedSoldier && damage.getIsCritical()) {
                 // Turn alliedSoldier to zombie
-            } else {
-                super.inflictDamage();
-            }
-
-        } else {
-            super.inflictDamage();
         }
     }
 
