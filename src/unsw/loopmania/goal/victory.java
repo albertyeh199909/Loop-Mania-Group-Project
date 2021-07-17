45593e3db5418c1f62a9346731a1d9a4f5f1e8b2
Zijian Yue <zijian.yue@student.unsw.edu.au>
2021-07-17 16:53:46 +1000

    Upload New File

---
+++ src/unsw/loopmania/goal/victory.java
@@ -0,0 +1,10 @@
+package unsw.loopmania.goals;
+import unsw.loopmania.world;
+
+public abstract class victory {
+   // check for a specific victory condtion
+   public Boolean checkVictoryCondition(Character c, world w)
+   {
+       return true;
+   }
+}
