8ad15dba224131fae461c1125744988fdbef0aee
s2424361895 <michael.j.shi@outlook.com>
2021-07-19 02:52:14 +1000

    added test for runTickMoves()

--- src/test/LoopManiaWorldTest.java
+++ src/test/LoopManiaWorldTest.java
@@ -15,7 +15,51 @@ import unsw.loopmania.Character;
 
 public class LoopManiaWorldTest {
     @Test
-    public void testRunBattles() {
+    public void testRunTickMoves() {
+        // Test details are shared with CharacterTest
+        List<Pair<Integer, Integer>> path = new ArrayList<Pair<Integer, Integer>>();
+        path.add(new Pair<Integer, Integer>(4,3));
+        path.add(new Pair<Integer, Integer>(4,2));
+        path.add(new Pair<Integer, Integer>(3,2));
+        path.add(new Pair<Integer, Integer>(3,1));
+        path.add(new Pair<Integer, Integer>(2,1));
+        path.add(new Pair<Integer, Integer>(1,1));
+        path.add(new Pair<Integer, Integer>(1,2));
+        path.add(new Pair<Integer, Integer>(1,3));
+        path.add(new Pair<Integer, Integer>(2,3));
+        path.add(new Pair<Integer, Integer>(3,3));
+
+        LoopManiaWorld world = new LoopManiaWorld(5,5, path);
+
+        world.runTickMoves();
+
+        PathPosition start = new PathPosition(1, path);
+        Character player = new Character(start);
+
+        assertNotNull(player);
+
+        world.setCharacter(player);
+
+        world.runTickMoves();
+
+        assertEquals(player.getX(), 3);
+        assertEquals(player.getY(), 2);
+
+        assertNotNull(player);
+
+        world.runTickMoves();
+
+        assertEquals(player.getX(), 3);
+        assertEquals(player.getY(), 1);
+
+        world.runTickMoves();
+        world.runTickMoves();
+
+        assertEquals(player.getX(), 1);
+        assertEquals(player.getY(), 1);
+
+
 
     }
+
 }
