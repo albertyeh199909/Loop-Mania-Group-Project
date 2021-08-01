d4ece9c405cb74742cf6e8feaeb3aa9983f1dbcb
Heng Peng Harington Chen <harington.chen@student.unsw.edu.au>
2021-08-02 04:24:03 +1000

    Update TreeStumpStrategy.java

--- src/unsw/loopmania/TreeStumpStrategy.java
+++ src/unsw/loopmania/TreeStumpStrategy.java
@@ -1,6 +1,10 @@
 package unsw.loopmania;
 
 public class TreeStumpStrategy implements Defense  {
+    /**
+    * Implements defense against bosses
+    * @param damage The damage class of the attack
+    */
     public void defense(DamageClass damage) {
         
         if(damage.getDamagedealer() instanceof Doggie || damage.getDamagedealer() instanceof ElanMuske) {
@@ -8,6 +12,11 @@ public class TreeStumpStrategy implements Defense  {
         }
         
     }
+
+    /**
+    * Implements crit damage of vampire
+    * @param damage The damage class of the attack
+    */
     public void specialEffect(DamageClass damage){
         if(damage.getDamagedealer() instanceof Vampire) {
             damage.setCriticalChance(damage.getCriticalChance()-6);
