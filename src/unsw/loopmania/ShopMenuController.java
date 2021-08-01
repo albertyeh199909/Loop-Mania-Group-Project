2e674308c18085eac7f5bfaf52cb6f7637b76439
s2424361895 <michael.j.shi@outlook.com>
2021-08-01 17:43:33 +1000

    added methods for every onAction for buy buttons

--- src/unsw/loopmania/ShopMenuController.java
+++ src/unsw/loopmania/ShopMenuController.java
@@ -1,6 +1,7 @@
 package unsw.loopmania;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 
 import java.io.IOException;
 
@@ -9,11 +10,78 @@ public class ShopMenuController {
 
     private LoopManiaWorld world;
 
+    @FXML
+    private Label outputFeedback;
+
     public void setGameSwitcher(MenuSwitcher gameSwitcher) {
         this.gameSwitcher = gameSwitcher;
     }
 
     public void setWorld(LoopManiaWorld world) {this.world = world; }
+
+    @FXML
+    private void buySword() {
+        if (world.purchase("sword")) {
+            outputFeedback.setText("Success!");
+        } else {
+            outputFeedback.setText("Sorry, you cannot do that yet...");
+        }
+    }
+
+    @FXML
+    private void buyStake() {
+        if (world.purchase("stake")) {
+            outputFeedback.setText("Success!");
+        } else {
+            outputFeedback.setText("Sorry, you cannot do that yet...");
+        }
+    }
+
+    @FXML
+    private void buyStaff() {
+        if (world.purchase("staff")) {
+            outputFeedback.setText("Success!");
+        }
+        else {
+            outputFeedback.setText("Sorry, you cannot do that yet...");
+        }
+    }
+
+    @FXML
+    private void buyArmour() {
+        if (world.purchase("armour")) {
+            outputFeedback.setText("Success!");
+        } else {
+            outputFeedback.setText("Sorry, you cannot do that yet...");
+        }
+    }
+
+    @FXML
+    private void buyShield() {
+        if (world.purchase("shield")) {
+            outputFeedback.setText("Success!");
+        } else {
+            outputFeedback.setText("Sorry, you cannot do that yet...");
+        }
+    }
+
+    @FXML
+    private void buyHelmet() {
+        if (world.purchase("helmet")) {
+            outputFeedback.setText("Success!");
+        } else {
+            outputFeedback.setText("Sorry, you cannot do that yet...");
+        }
+    }
+
+    @FXML
+    private void buyPotion() {
+        if (world.purchase("potion")) {
+            outputFeedback.setText("Success!");
+        } else {
+            outputFeedback.setText("Sorry, you cannot do that yet...");
+        }
+    }
 
     @FXML
     private void returnToGame() throws IOException {
